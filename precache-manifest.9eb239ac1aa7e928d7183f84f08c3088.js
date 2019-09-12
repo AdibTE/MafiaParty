9347422e168172c1b8a333cb1dc2d2f71f72072e
AdibTE <46872835+AdibTE@users.noreply.github.com>
2019-09-13 04:16:13 +0430

    Add files via upload

---
+++ precache-manifest.9eb239ac1aa7e928d7183f84f08c3088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eeb58aad1c8edeee07b096e24b7f573",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80ce35a95c4cb220fa36",
+    "url": "/static/css/main.fc0517a0.chunk.css"
+  },
+  {
+    "revision": "e9438265e7712876d0fe",
+    "url": "/static/js/2.d883074c.chunk.js"
+  },
+  {
+    "revision": "80ce35a95c4cb220fa36",
+    "url": "/static/js/main.52dca2ee.chunk.js"
+  },
+  {
+    "revision": "26e5908e2fd62aa3c6b2",
+    "url": "/static/js/runtime~main.0bced257.js"
+  },
+  {
+    "revision": "6d293d225364e06ee1d06da676d5ab58",
+    "url": "/static/media/logo.6d293d22.png"
+  }
+]);
